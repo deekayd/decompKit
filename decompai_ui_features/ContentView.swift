c303910ffab44d05b96e74adf0f0a87c7bb7e323
Dmitry Kadyrov <deekayd@icloud.com>
2026-06-09 10:34:49 +0400

    Initial Commit

---
+++ decompai_ui_features/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  decompai_ui_features
+//
+//  Created by Dmitry Kadyrov on 09/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ decompai_ui_features/decompai_ui_featuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  decompai_ui_featuresApp.swift
+//  decompai_ui_features
+//
+//  Created by Dmitry Kadyrov on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct decompai_ui_featuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
